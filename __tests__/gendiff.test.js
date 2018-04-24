102922f1b92e2482f258ff80b5b1102dcd9dbac3
Andrey Vershinin <adw006@ya.ru>
2018-04-24 21:20:11 +0300

    fix test, var name, make some refactoring

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,13 +1,11 @@
-import * as fs from 'fs';
+import fs from 'fs';
 import genDiff from '../src';
 
-const pathToFile1 = '__tests__/__fixtures__/before.json';
-const pathToFile2 = '__tests__/__fixtures__/after.json';
-const pathToResFile = '__tests__/__fixtures__/beforeafter.test.res';
-
-const resBeforeAfter = fs.readFileSync(pathToResFile, 'utf8');
-const diff = genDiff(pathToFile1, pathToFile2);
-
 test('diff to equal resBeforeAfter', () => {
+  const pathToFile1 = '__tests__/__fixtures__/before.json';
+  const pathToFile2 = '__tests__/__fixtures__/after.json';
+  const pathToResFile = '__tests__/__fixtures__/beforeafter.test.res';
+  const resBeforeAfter = fs.readFileSync(pathToResFile, 'utf8');
+  const diff = genDiff(pathToFile1, pathToFile2);
   expect(diff).toBe(resBeforeAfter);
 });
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import * as fs from 'fs';
+import fs from 'fs';
 import _ from 'lodash';
 
 export default (source1, source2) => {
@@ -6,25 +6,24 @@ export default (source1, source2) => {
   const fileContent2 = fs.readFileSync(source2, 'utf8');
   const obj1 = JSON.parse(fileContent1);
   const obj2 = JSON.parse(fileContent2);
-  const resultKeys = Object.keys({ ...obj1, ...obj2 });
+  const allKeys = Object.keys({ ...obj1, ...obj2 });
 
   const resultBuilder = (acc, key) => {
     const inObj1 = _.has(obj1, key);
     const inObj2 = _.has(obj2, key);
+    let currentKeyRes;
+
     if (obj1[key] === obj2[key]) {
-      return `${acc}\n    ${key}: ${obj1[key]}`;
-    }
-    if (inObj1 && inObj2) {
-      return `${acc}\n  + ${key}: ${obj2[key]}\n  - ${key}: ${obj1[key]}`;
+      currentKeyRes = `  ${key}: ${obj1[key]}`;
+    } else if (inObj1 && inObj2) {
+      currentKeyRes = `+ ${key}: ${obj2[key]}\n  - ${key}: ${obj1[key]}`;
     } else if (inObj1) {
-      return `${acc}\n  - ${key}: ${obj1[key]}`;
+      currentKeyRes = `- ${key}: ${obj1[key]}`;
+    } else {
+      currentKeyRes = `+ ${key}: ${obj2[key]}`;
     }
-    return `${acc}\n  + ${key}: ${obj2[key]}`;
+    return `${acc}\n  ${currentKeyRes}`;
   };
 
-  const resultString = `{${resultKeys.reduce(resultBuilder, '')}\n}`;
-  // console.log(obj1);
-  // console.log(obj2);
-  // console.log(resultString);
-  return resultString;
+  return `{${allKeys.reduce(resultBuilder, '')}\n}`;
 };
