27a7b30afd6181cc50f0c3fbab335f666932f10e
Andrey Vershinin <adw006@ya.ru>
2018-04-25 08:35:53 +0300

    add js-yaml & test for yaml

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,9 +1,18 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-test('diff to equal resBeforeAfter', () => {
+test('Diff to equal resBeforeAfter <JSON format>', () => {
   const pathToFile1 = '__tests__/__fixtures__/before.json';
   const pathToFile2 = '__tests__/__fixtures__/after.json';
+  const pathToResFile = '__tests__/__fixtures__/beforeafter.test.res';
+  const resBeforeAfter = fs.readFileSync(pathToResFile, 'utf8');
+  const diff = genDiff(pathToFile1, pathToFile2);
+  expect(diff).toBe(resBeforeAfter);
+});
+
+test('Diff to equal resBeforeAfter <YAML format>', () => {
+  const pathToFile1 = '__tests__/__fixtures__/before.yml';
+  const pathToFile2 = '__tests__/__fixtures__/after.yml';
   const pathToResFile = '__tests__/__fixtures__/beforeafter.test.res';
   const resBeforeAfter = fs.readFileSync(pathToResFile, 'utf8');
   const diff = genDiff(pathToFile1, pathToFile2);
