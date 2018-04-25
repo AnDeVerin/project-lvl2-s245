2c99b84e0aef0bc64e46283b664a628852ecc662
Andrey Vershinin <adw006@ya.ru>
2018-04-25 21:52:48 +0300

    add ini parser & test files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -18,3 +18,12 @@ test('Diff to equal resBeforeAfter <YAML format>', () => {
   const diff = genDiff(pathToFile1, pathToFile2);
   expect(diff).toBe(resBeforeAfter);
 });
+
+test('Diff to equal resBeforeAfter <INI format>', () => {
+  const pathToFile1 = '__tests__/__fixtures__/before.ini';
+  const pathToFile2 = '__tests__/__fixtures__/after.ini';
+  const pathToResFile = '__tests__/__fixtures__/beforeafter.test.res';
+  const resBeforeAfter = fs.readFileSync(pathToResFile, 'utf8');
+  const diff = genDiff(pathToFile1, pathToFile2);
+  expect(diff).toBe(resBeforeAfter);
+});
