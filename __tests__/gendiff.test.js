1bbaa5e0536729229d8edb828a596bb406d0bebb
Andrey Vershinin <adw006@ya.ru>
2018-04-28 19:36:03 +0300

    add tests files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,11 +1,26 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-test('genDiff to equal result from "diff-output.test.res" file', () => {
+const pathToResFile = '__tests__/__fixtures__/diff-output.test.res';
+const diffFromFile = fs.readFileSync(pathToResFile, 'utf8');
+
+test('genDiff <JSON> to equal result from "diff-output.test.res" file', () => {
   const pathToFile1 = '__tests__/__fixtures__/before.json';
   const pathToFile2 = '__tests__/__fixtures__/after.json';
-  const pathToResFile = '__tests__/__fixtures__/diff-output.test.res';
-  const diffFromFile = fs.readFileSync(pathToResFile, 'utf8');
+  const diff = genDiff(pathToFile1, pathToFile2);
+  expect(diff).toBe(diffFromFile);
+});
+
+test('genDiff <INI> to equal result from "diff-output.test.res" file', () => {
+  const pathToFile1 = '__tests__/__fixtures__/before.ini';
+  const pathToFile2 = '__tests__/__fixtures__/after.ini';
+  const diff = genDiff(pathToFile1, pathToFile2);
+  expect(diff).toBe(diffFromFile);
+});
+
+test('genDiff <YAML> to equal result from "diff-output.test.res" file', () => {
+  const pathToFile1 = '__tests__/__fixtures__/before.yml';
+  const pathToFile2 = '__tests__/__fixtures__/after.yml';
   const diff = genDiff(pathToFile1, pathToFile2);
   expect(diff).toBe(diffFromFile);
 });
---
+++ src/json-ini-converter.js
@@ -0,0 +1,5 @@
+// var fs = require('fs'), ini = require('ini');
+
+// var config = JSON.parse(fs.readFileSync('./after.json', 'utf-8'));
+
+// fs.writeFileSync('./after.ini', ini.stringify(config, { section: 'section' }));
